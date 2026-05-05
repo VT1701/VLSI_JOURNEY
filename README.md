7bb4b815b61d21b8eb95e5be444762b578fb7d51
Vishwa Trivedi <vishwatrivedi0@gmail.com>
2026-05-05 21:23:58 +0530

    Moore FSM 1011 detector

---
+++ README.md
@@ -0,0 +1,4 @@
+# VLSI Journey - Day 2
+
+Built a Moore FSM that detects pattern '1011' in a serial bitstream.
+Verified working with testbench - detects overlapping patterns correctly.
---
+++ tb_moore_1011.v
@@ -0,0 +1,29 @@
+module tb;
+  reg clk,rst,in;
+  wire out;
+  moore dut(.clk(clk),.rst(rst),.in(in),.out(out));
+  initial 
+    clk = 0;
+  always
+    #5 clk=~clk;
+  initial 
+    begin
+      $monitor("Time=%0t | rst=%b , in=%b , state=%b , out=%b",$time,rst,in,dut.state,out);
+      rst = 1;
+      in = 0;
+      
+      #15 rst = 0;
+      #10 in = 1;
+      #10 in = 0;
+      #10 in = 1;
+      #10 in = 1;
+      #10 in = 1;
+      #10 in = 0;
+      #10 in = 1;
+      #10 in = 1;
+      
+      #20 $finish;
+    end
+endmodule
+      
+      
